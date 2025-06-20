bd5a92ce3b1873862c002a674ab1917265e7cdb0
Chrabon Dey Sarker <chrabondey@gmail.com>
2025-06-20 23:05:12 +0600

    here i pass

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+   <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {/* If Navbar uses dynamic values, wrap with suppressHydrationWarning */}
